d22ca66950f6754ac83b4d4b68150ff7f8de8139
Rickard Luu <rickardimine@gmail.com>
2023-01-25 16:52:39 +0100

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+function weatherBalloon(cityID) {
+  var key = "yofc78555d484a3a86058bec525cab5f27";
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?id=" +
+      cityID +
+      "&appid=" +
+      key
+  )
+    .then(function (resp) {
+      return resp.json();
+    }) // Convert data to json
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function () {
+      // catch any errors
+    });
+}
+
+window.onload = function () {
+  weatherBalloon(6167865);
+};
+
+function drawWeather(d) {
+  var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+  var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+
+  document.getElementById("description").innerHTML = d.weather[0].description;
+  document.getElementById("temp").innerHTML = celcius + "&deg;";
+  document.getElementById("location").innerHTML = d.name;
+}
