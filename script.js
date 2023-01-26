ec8b4e9677e2eec7d21611c3885a5fce735d15fd
Rickard Luu <rickardimine@gmail.com>
2023-01-26 10:40:36 +0100

    update

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function weatherBalloon(cityID) {
     }) // Convert data to json
     .then(function (data) {
       drawWeather(data); // Call drawWeather
+      console.log(data);
     })
     .catch(function () {
       // catch any errors
@@ -18,14 +19,49 @@ function weatherBalloon(cityID) {
 }
 
 window.onload = function () {
-  weatherBalloon(6167865);
+  weatherBalloon(1850147);
+  //2692969
+  //1850147
+  //292223
 };
 
 function drawWeather(d) {
   var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
   var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
+  var description = d.weather[0].description;
 
-  document.getElementById("description").innerHTML = d.weather[0].description;
+  document.getElementById("description").innerHTML = description;
   document.getElementById("temp").innerHTML = celcius + "&deg;";
   document.getElementById("location").innerHTML = d.name;
+
+  if (description.indexOf("rain") > 0) {
+    document.body.className = "rainy";
+  } else if (description.indexOf("cloud") > 0) {
+    document.body.className = "cloudy";
+  } else if (description.indexOf("sunny") > 0) {
+    document.body.className = "sunny";
+  }
 }
+
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["CityName"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [5],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
